d09481c8d3e21683062b54b0ec6a298b3f4e59fc
NikitaNpl <totmyanin.n@bk.ru>
2020-10-04 19:22:08 +0300

    [add] - добавил весь оставшийся функционал

--- src/App.js
+++ src/App.js
@@ -1,72 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 import Cards from './components/Cards'
 
-import img12 from './assets/img/12.jpg';
-import img13 from './assets/img/13.jpg';
-import img14 from './assets/img/14.jpg';
-import imgProfile from './assets/img/15.jpg'
-const cards = [
-  {
-    'id': 1, 
-    'name': 'Niple',
-    'image': imgProfile,
-    'location': 'Russia, Podolsk', 
-    'shootingTime': '1h', 
-    'picture': img12,
-    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  {
-    'id': 2, 
-    'name': 'Maxim',
-    'image': imgProfile, 
-    'location': 'Russia, Moscow', 
-    'shootingTime': '3h', 
-    'picture': img13,
-    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  {
-    'id': 3, 
-    'name': 'Nikita',
-    'image': imgProfile,
-    'shootingTime': '2s', 
-    'picture': img14,
-    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  {
-    'id': 1, 
-    'name': 'Niple',
-    'image': imgProfile,
-    'location': 'Russia, Podolsk', 
-    'shootingTime': '1h', 
-    'picture': img12,
-    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  {
-    'id': 2, 
-    'name': 'Maxim',
-    'image': imgProfile, 
-    'location': 'Russia, Moscow', 
-    'shootingTime': '3h', 
-    'picture': img13,
-    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  {
-    'id': 3, 
-    'name': 'Nikita',
-    'image': imgProfile,
-    'shootingTime': '2s', 
-    'picture': img14,
-    'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  },
-  
-]
+const apiKey = '9781492bddf356d5157ba5a69a1b13ea';
+const perPage = 10;
+const apiUrlFromTopPhotos = `https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${apiKey}&per_page=${perPage}&format=json&nojsoncallback=1`;
 
 function App() {
+  const [idPhotos, setIdPhotos] = useState(null);
+  const [infoPhotos, setInfoPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    axios.get(apiUrlFromTopPhotos)
+    .then((response) => {
+      if (response.status === 200) {
+        const arrayPhoto = response.data.photos.photo;
+        let idPhotos = arrayPhoto.map((item) =>  item.id);
+        setIdPhotos(idPhotos);
+      }
+      return;
+    })
+    .catch(() => {
+      alert('Не удалось выгрузить фото!');
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!idPhotos) {
+      return;
+    }
+    idPhotos.map((item) => {
+      axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=${apiKey}&photo_id=${item}&format=json&nojsoncallback=1`)
+      .then((response) =>  {
+        if(response.status === 200) {
+          let {id, secret, server, farm, owner, description, dates, views, location} = response.data.photo;
+          setInfoPhotos(lastInfo => [...lastInfo, {id, secret, server, farm, owner, description, dates, views, location}])
+        }
+      }).then(() => setIsLoading(true));
+    })
+  }, [idPhotos]);
+  
   return (
     <>
       <div className="list-cards">
-        <Cards cards={cards}/>
+        {isLoading && infoPhotos.length === perPage ? <Cards cards={infoPhotos}/> : 'Загрузка....'}
       </div>
     </>
   );
--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -3,35 +3,79 @@ import React from 'react';
 import heart from '../../assets/img/love.svg';
 import './index.scss';
 
+
+
 const Cards = ({cards}) => {
-  console.log(cards)
+    console.log(cards)
+
+
+    function getTimePosted(currentDate) {
+      let differenceDate = getTimeDifference(currentDate);
+      return dateConverter(differenceDate);
+    }
+
+    function getTimeDifference(currentDate) {
+      let pastDate = new Date(currentDate.toString())
+      let differenceDate = new Date().getTime() - pastDate.getTime();
+      
+      return (new Date(differenceDate));
+    }
+
+    function dateConverter(date) {
+      let month = date.getMonth();
+      let days = date.getDate();
+      let hours = date.getHours();
+      let min = date.getMinutes();
+      let sec = date.getSeconds();
+
+      if (month) {
+        return month + 'mo';
+      } if (days) {
+        return days + 'd';
+      } if (hours) {
+        return hours + 'h'
+      } if (min) {
+        return min + 'min';
+      } if (sec) {
+        return sec + 's'
+      }
+
+      return;
+    }
+
     return (
       <>
         {cards.map((item, index) => (
           <div className="card" key={index}>
             <div className="card__header">
               <div className="card__header-profile-image">
-                <img src={item.image} alt="prof-pic"/>
+                <img src={`http://farm${item.owner.iconfarm}.staticflickr.com/${item.owner.iconserver}/buddyicons/${item.owner.nsid}.jpg`}
+                  alt="prof-pic"
+                />
               </div>
               <div className="card__header-profile-info">
                 <div className="info__name-location">
-                  <span className="name">{item.name}</span>
-                  {item.location && <span className="location">{item.location}</span>}
+                  <span className="name">{item.owner.username}</span>
+                  {item.location && <span className="location">{item.location.country['_content']}</span>}
                 </div>
-                <span className="info__shooting-time">{item.shootingTime}</span>
+                <span className="info__shooting-time">{getTimePosted(item.dates.taken)}</span>
               </div>
             </div>
 
-            <div className="card__picture">
-              <img src={item.picture} alt="timg12"/>
+             <div className="card__picture">
+              <img src={`http://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`} alt="timg12"/>
             </div>
 
             <div className="card__info">
               <div className="info-likes">
-                <img src={heart} alt="heart"/>
-                <span>12 345</span>
+                <img 
+                  onClick={() => alert(`ID записи: ${index}`)} 
+                  src={heart} 
+                  alt="heart"
+                />
+                <span>{item.views.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</span>
               </div>
-                {item.text && <div className="info-text">{item.text}</div>}
+                {item.description && <div className="info-text">{item.description['_content']}</div>}
             </div>
           </div>
         ))}
